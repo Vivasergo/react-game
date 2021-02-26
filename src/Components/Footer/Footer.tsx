93b869e2683ee7bfb4cf2577b419b797f3e43ca6
Serhii Vasilikhin <vivasergo@gmail.com>
2021-02-26 08:58:14 +0100

    fix: implement correct link actions

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NavLink } from "react-router-dom"
 import logo from "../../Img/rs_school_js.png"
 import style from "./footer.module.css"
 
@@ -8,11 +7,11 @@ export const Footer = () => {
     return (
 
         <footer className={style.footer} >
-            <NavLink to="https://rs.school/js/" >
+            <a href="https://rs.school/js/" >
                 <img className={style.footerImg} src={logo} alt="logo" />
-            </NavLink>
+            </a>
             <span className="mx-3">February, 2021</span>
-            <NavLink to="https://github.com/Vivasergo">Author's GitHub</NavLink>
+            <a href="https://github.com/Vivasergo">Author's GitHub</a>
         </footer>
 
 
